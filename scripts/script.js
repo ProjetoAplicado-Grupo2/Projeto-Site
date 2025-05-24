2b22ee1bb33b77cb062e73b4338b057b184aedc8
RicardoDMAssis <ricardo.assis@undf.edu.br>
2025-05-24 17:00:53 -0300

    reorganizacao das paginas e separacao de arquivos

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+/// CONFIGURAÇÃO DO MENU LATERAL & MENU SACOLÃO
+
+const menuLateral = document.getElementById("menu-lateral")
+const menuSacola = document.getElementById("menu-sacola")
+
+function abrirMenu(){
+    menuLateral.classList.add("ativo")
+}
+
+function fecharMenu(){
+    menuLateral.classList.remove("ativo")
+}
+
+function abrirSacola(){
+    menuSacola.classList.add("ativo")
+}
+
+function fecharSacola(){
+    menuSacola.classList.remove("ativo")
+}
