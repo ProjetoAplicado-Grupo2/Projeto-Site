7aaa85a1979041b9b96c8818e3a4650fccb06233
RicardoDMAssis <ricardo.assis@undf.edu.br>
2025-06-06 15:04:30 -0300

    pagina produto com api

---
+++ scripts/produto.js
@@ -0,0 +1,36 @@
+const URL_PRODUTOS = "https://fakestoreapi.com/products/"
+const parametros = new URLSearchParams(window.location.search)
+let idProduto = parametros.get("id")
+
+const esquerda = document.getElementsByClassName("esquerda")
+const imagemProduto = document.getElementById("imagemProduto")
+const nomeProduto = document.getElementById("nomeProduto")
+const precoProduto = document.getElementById("precoProduto")
+
+
+if(idProduto){
+    console.log("Entrou " + idProduto)
+    async function acharProduto() {
+        try{
+            const response = await fetch(URL_PRODUTOS+idProduto)
+            const produto = await response.json()
+
+            nomeProduto.textContent = produto.title
+            imagemProduto.src= produto.image
+            precoProduto.textContent = "R$ " + produto.price
+        } catch(error) {
+            console.log("Erro ao achar o produto. CÓDIGO: " + error)
+        }
+    }
+
+    acharProduto()
+
+} else{
+    alert("ID do Produto não encontrado na Url. Colocaremos um outro produto")
+    idProduto = 1;
+}
+
+
+function comprar(){
+    console.log("Comprado")
+}
